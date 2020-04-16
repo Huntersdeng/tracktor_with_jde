beddc3cebbe4e4d230ad0a83b3056a77b8ed18cf
Huntersdeng <1050130028@qq.com>
2020-04-16 12:00:47 +0800

    first commit

--- tracktor.py
+++ tracktor.py
@@ -15,13 +15,13 @@ import torchvision
 from torchvision.transforms import transforms as T
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
-from jde_rcnn import Jde_RCNN
+from model import Jde_RCNN
 from tracker import Tracker
 from utils.utils import interpolate, plot_sequence, get_mot_accum, evaluate_mot_accums, write_results
 from utils.datasets import LoadImagesAndLabels
 
 os.environ['CUDA_VISIBLE_DEVICES']='0'
-root = 'data/dgw/'
+root = '/data/dgw/'
 output_dir = './output'
 
 
