590d449fbe90f3d59a84e33240a688e676569b1f
Huntersdeng <1050130028@qq.com>
2020-04-17 17:43:07 +0800

    first commit

--- a.py
+++ a.py
@@ -5,7 +5,7 @@ from model import Jde_RCNN
 backbone = resnet_fpn_backbone('resnet50', True)
 backbone.out_channels = 256
 
-model = Jde_RCNN(backbone, num_ID=10, min_size=300, max_size=400, version='v1')
+model = Jde_RCNN(backbone, num_ID=10, min_size=300, max_size=400, version='v2')
 
 imgs = torch.rand((1,3,300,400))
 boxes = torch.Tensor([[20,30,40,50]])
