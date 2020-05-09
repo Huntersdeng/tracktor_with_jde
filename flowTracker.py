0bf560f05f64c33b70b097b4c5125061ff5e7c86
Huntersdeng <1050130028@qq.com>
2020-05-09 15:51:43 +0800

    first commit

--- flowTracker.py
+++ flowTracker.py
@@ -19,10 +19,6 @@ class flowTracker(nn.Module):
             boxes, target = match(boxes, target)
 
         box_feature = [self.box_roi_pool(feature[:,:,int(box[1]):int(box[3]),int(box[0]):int(box[2])]) for box in boxes]
-        # box_feature = []
-        # for box in boxes:
-        #     x0, y0, x1, y1 = int(box[0]), int(box[2]), int(box[1]), int(box[3])
-        #     box_feature.append(self.box_roi_pool(feature[:,:,x0:x1,y0:y1]))
         box_feature = torch.cat(box_feature, dim=0)
         box_feature = box_feature.flatten(start_dim=1)
         deltaB = F.relu(self.fc(box_feature))
@@ -33,8 +29,7 @@ class flowTracker(nn.Module):
         return deltaB + boxes
 
 def match(boxes, target):
-    # boxes = boxes[np.argsort(boxes[:,0])]
-    # target = target[np.argsort(target[:,0])]
+    
     m, n = boxes.shape[0], target.shape[0]
     idx = torch.zeros((m, n))
     for i in range(m):
@@ -42,8 +37,8 @@ def match(boxes, target):
             if boxes[i,1]==target[j,1]:
                 idx[i,j] = 1
                 break
-    boxes = boxes[idx.sum(dim=0, dtype=torch.uint8)]
-    target = target[idx.sum(dim=1, dtype=torch.uint8)]
+    boxes = boxes[idx.sum(dim=1, dtype=torch.uint8)]
+    target = target[idx.sum(dim=0, dtype=torch.uint8)]
     boxes = boxes[torch.argsort(boxes[:,1])]
     target = target[torch.argsort(target[:,1])]
     boxes = boxes[:,2:6]
